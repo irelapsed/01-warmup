62e11c34113e84204cec8afe3f55e077ea29fcab
Danil B <blnvdanil@gmail.com>
2023-05-07 20:47:08 +0300

    Ready! Set! Go!

---
+++ src/warmup.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function abProblem(a, b) {
+}
+
+function centuryByYearProblem(year) {
+}
+
+function colorsProblem(hexColor) {
+}
+
+function fibonacciProblem(n) {
+}
+
+function matrixProblem(matrix) {
+}
+
+function numberSystemProblem(n, targetNs) {
+}
+
+function phoneProblem(phoneNumber) {
+}
+
+function smilesProblem(text) {
+}
+
+function ticTacToeProblem(field) {
+}
+
+module.exports = {
+  abProblem,
+  centuryByYearProblem,
+  colorsProblem,
+  fibonacciProblem,
+  matrixProblem,
+  numberSystemProblem,
+  phoneProblem,
+  smilesProblem,
+  ticTacToeProblem
+};
